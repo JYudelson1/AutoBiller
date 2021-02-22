90ca47dce6b2f23880ec9d707d0e0405f9c72847
Joey <jyudel@gmail.com>
2021-02-22 15:13:50 -0500

    minor fix

--- AutoBiller/uiComponents.py
+++ AutoBiller/uiComponents.py
@@ -248,7 +248,7 @@ class LoginConfirmationPopup(QDialog):
 
     def verify_code(self, device, code):
         if not self.icloud_acct.validate_verification_code(device, code):
-            #print("Failed to verify verification code")
+
             warning = QMessageBox.warning(self,
                                     'Invalid Code',
                                     "The verification code you entered was not valid! Try logging in again!")
